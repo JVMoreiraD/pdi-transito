c03287ed8ade75ccccf851095aace7cb2c694f9e
joao <joaovfranca@hotmail.com>
2022-02-07 19:13:29 -0300

    Main: changed the Dataset folder name

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print('Vai dar certo')
+import cv2 as cv
+import sys
+img = cv.imread(cv.samples.findFile("Dataset/1.jpg"))
+if img is None:
+    sys.exit("Could not read the image.")
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
