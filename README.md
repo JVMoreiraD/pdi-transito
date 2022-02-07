34ceffeedbe07f0c066e9eaa40be620d73718f55
João Vitor <jvitormoreirad@hotmail.com>
2022-02-07 20:20:19 -0300

    added readme and mask for color red

---
+++ README.md
@@ -0,0 +1,10 @@
+## Trabalho de PDI
+
+### Implementação do artigo Processamento Digital de Imagens Aplicado a Identificação Automática de Placas de Trânsito.
+
+## Metodologia
+
+### 1. Segmentar a imagem de entrada com o objetivo de ressaltar uma porção desta imagem que se encontre na mesma faixa de cores da placa a ser localizada.
+
+### 2. Processar o objeto identificado para verificar se ele pode ser caracterizado como uma placa de pare.
+
--- src/main.py
+++ src/main.py
@@ -1,9 +1,39 @@
 import cv2 as cv
+import numpy as np
 import sys
-img = cv.imread(cv.samples.findFile("Dataset/1.jpg"))
-if img is None:
+
+imgRGB = cv.imread(cv.samples.findFile("Dataset/1.jpg"))
+
+if imgRGB is None:
     sys.exit("Could not read the image.")
-cv.imshow("Display window", img)
+
+cv.imshow("Display window RGB", imgRGB)
+
+imgHSV = cv.cvtColor(imgRGB, cv.COLOR_BGR2HSV)
+
+result = imgHSV.copy()
+
+# Convert to HSV
+
+cv.imshow("Display window HSV", imgHSV)
+
 k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+
+# lower boundary RED color range values; Hue (0 - 10)
+lower1 = np.array([0, 100, 20])
+upper1 = np.array([10, 255, 255])
+ 
+# upper boundary RED color range values; Hue (160 - 180)
+lower2 = np.array([160,100,20])
+upper2 = np.array([179,255,255])
+ 
+lower_mask = cv.inRange(imgHSV, lower1, upper1)
+upper_mask = cv.inRange(imgHSV, lower2, upper2)
+
+full_mask = lower_mask + upper_mask
+
+cv.imshow('full-mask',full_mask)
+
+ 
+cv.waitKey(0)
+cv.destroyAllWindows()
