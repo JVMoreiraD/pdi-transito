197f13b59348e4bdf7489b087af56de6b3d30a38
João Vitor <jvitormoreirad@hotmail.com>
2022-02-07 22:16:19 -0300

    updated readme and main.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Trabalho de PDI
+# Trabalho de PDI
 
-### Implementação do artigo Processamento Digital de Imagens Aplicado a Identificação Automática de Placas de Trânsito.
+## Implementação do artigo Processamento Digital de Imagens Aplicado a Identificação Automática de Placas de Trânsito.
 
 ## Metodologia
 
@@ -8,3 +8,17 @@
 
 ### 2. Processar o objeto identificado para verificar se ele pode ser caracterizado como uma placa de pare.
 
+### **To-do**
+
+- [x] Converter imagem para de RGB para HSV.
+- [x] Verificar aonde existe predominacia de pixels vermelhos.
+- [ ] Recortar a imagem gerada aonde existe os pixels vermelhos se comporem, no minimo 10% da imagem original.
+- [ ] Descartar objetos vermelhos de dimensão inferior a 10% da imagem.
+- [ ] Aplicar filtro de Sobel para identificar as bordas do objeto.
+- [ ] Dilatar imagem gerada para realizar junção das bordas de um mesmo objeto.
+- [ ] Preencher o interior da placa.
+- [ ] Remover qualquer objeto conectado que esteja em contato com qualquer extremidade da imagem de entrada.
+- [ ] Aplicar erosão com auxilio de uma funçao similar a imerode, visando uma imagem mais bem definida do objeto de intersse.
+- [ ] Aplicar imagem gerada nas etapas anteriores em um algoritmo de retas pela transformada de Hough.
+- [ ] Usar detector de bordas ótimo de Canny
+- [ ] Fazer o resto da To-Do
--- src/main.py
+++ src/main.py
@@ -1,37 +1,37 @@
 import cv2 as cv
 import numpy as np
+# import PIL
 import sys
 
-imgRGB = cv.imread(cv.samples.findFile("Dataset/1.jpg"))
+imgRGB = cv.imread(cv.samples.findFile("Dataset/3.jpg"))
 
+# Checks if the image exist
 if imgRGB is None:
     sys.exit("Could not read the image.")
 
-cv.imshow("Display window RGB", imgRGB)
-
-imgHSV = cv.cvtColor(imgRGB, cv.COLOR_BGR2HSV)
-
-result = imgHSV.copy()
+# Resize in case of a image being too big.
+imgRGBs = cv.resize(imgRGB, (940, 540))
 
 # Convert to HSV
+imgHSV = cv.cvtColor(imgRGBs, cv.COLOR_BGR2HSV)
 
-cv.imshow("Display window HSV", imgHSV)
-
-k = cv.waitKey(0)
-
-# lower boundary RED color range values; Hue (0 - 10)
+# Lower boundary RED color range values; Hue (0 - 10)
 lower1 = np.array([0, 100, 20])
 upper1 = np.array([10, 255, 255])
  
-# upper boundary RED color range values; Hue (160 - 180)
-lower2 = np.array([160,100,20])
+# Upper boundary RED color range values; Hue (160 - 180)
+lower2 = np.array([160,100,20]) 
 upper2 = np.array([179,255,255])
  
+ # Apply masks in imageHSV
 lower_mask = cv.inRange(imgHSV, lower1, upper1)
-upper_mask = cv.inRange(imgHSV, lower2, upper2)
+upper_mask = cv.inRange(imgHSV, lower2, upper2) 
 
 full_mask = lower_mask + upper_mask
 
+# Show images
+cv.imshow("Display window HSV", imgHSV)
+cv.imshow("Display window RGB", imgRGBs)
 cv.imshow('full-mask',full_mask)
 
  
